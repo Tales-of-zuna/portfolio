6a793048f541da2a9d2bf0238083652f8de48e54
Zuunbileg <zuunbileg.b@unitel.mn>
2023-08-14 08:15:14 +0800

    initial

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen bg-stone-200 flex justify-center items-center">
+    <div
+      class="px-8 py-4 bg-white font-semibold text-stone-700 transition-all transform duration-300 hover:scale-105 hover:-translate-y-4 hover:shadow-2xl rounded-xl text-6xl shadow-lg"
+    >
+      Hello world!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
