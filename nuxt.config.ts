da74ae2b7eface0e21bbe0a943578f5a8d839415
Zuunbileg <zuunbileg.b@unitel.mn>
2023-08-14 19:36:26 +0800

    hello

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/portfolio/", // baseURL: '/<repository>/'
+    buildAssetsDir: "assets",
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "dayjs-nuxt"],
 });
